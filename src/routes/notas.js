d0b6b3a2a8bc9708137382874570bea47adea1b2
stefany <stefanyro@users.noreply.github.com>
2020-04-01 21:59:10 +0200

    correcion de una consulta

--- src/routes/notas.js
+++ src/routes/notas.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pool = require('../database')
 
 router.get('/',async(req,res)=>{
-    const user = await pool.query('select * from notas')
+    const user = await pool.query('SELECT e.nombre,d.nombre, m.materia,p.periodo,n.nota FROM estudiantes e, docentes d, materias m, periodo_academico p, notas n WHERE e.id_estudiante=n.id_estudiante AND d.id_docente=n.id_docente AND m.id_materia=n.id_materia AND p.id_periodo=n.id_periodo')
     res.render('notas/consultar',{user});
 });
 module.exports=router;
